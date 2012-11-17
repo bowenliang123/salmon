8782a5b00e0721b122989fe9426c036bf0994b3e
Bowen Liang <simonlbw@gmail.com>
2012-11-17 22:13:38 +0800

    新增验证Conn的逻辑。并优化代码。

--- src/sardine_submitter.erl
+++ src/sardine_submitter.erl
@@ -6,7 +6,7 @@
 %%
 %% Include files
 %%
--include("../include/sardine_topoconfig_interface.hrl").
+-include("../include/sardine_config_interface.hrl").
 
 %%
 %% Exported Functions
@@ -26,33 +26,60 @@ submitTopology(Cluster, Topo) when is_record(Topo, topoConfig) ->
 %%
 parseConns(Topo) when is_record(Topo, topoConfig) ->
 	#topoConfig{spouts = SpoutsConfigs, bolts = BoltsConfigs, conns = ConnsConfigs} = Topo,
-%% 	ConnsConfigs1 = valiadConns(ConnsConfigs).
-	getBoltsIdList(BoltsConfigs).
+	VerifiedConnPairsList = verifyConns(ConnsConfigs, SpoutsConfigs, BoltsConfigs).
 
-valiadConns(ConnsConfigs) when is_list(ConnsConfigs) ->
-	ConnPairsList = getConnPairsList(ConnsConfigs).
+
+verifyConns(ConnsConfigs, SpoutsConfigs, BoltsConfigs) when is_list(ConnsConfigs) ->
+	SpoutsIdList = getSpoutsIdList(SpoutsConfigs),
+	BoltsIdList = getBoltsIdList(BoltsConfigs),
+	verifyEachConnPair(ConnsConfigs, SpoutsIdList, BoltsIdList, []).
+
+
+
+verifyEachConnPair([H|T] = ConnsConfigs, SpoutsIdList, BoltsIdList, ResultConnsConfigsList)
+  when is_record(H, connConfig)->
+	#connConfig{from = From, to = To} = H,
+	if
+		From==""; To==""->
+			verifyEachConnPair(T, SpoutsIdList, BoltsIdList, ResultConnsConfigsList);
+		true->
+			Flag1 = sardine_common:isInList(From, lists:append([SpoutsIdList,BoltsIdList])),
+			Flag2 = sardine_common:isInList(To, BoltsIdList),
+			if
+				Flag1 and Flag2 ->
+					verifyEachConnPair(T, SpoutsIdList, BoltsIdList, [H|ResultConnsConfigsList]);
+				true ->
+					verifyEachConnPair(T, SpoutsIdList, BoltsIdList, ResultConnsConfigsList)
+			end
+   end;
+verifyEachConnPair([], _, _, ResultConnsConfigsList)->
+	ResultConnsConfigsList.
+
+verifyConnPair({From, To}, SpoutsIdList, BoltsIdList) ->
+	ok.
+	
 
 getSpoutsIdList(SpoutsConfigs) ->
 	getSpoutsIdList(SpoutsConfigs,[]).
-getSpoutsIdList([],ResultList) ->
-	ResultList;
+
 getSpoutsIdList([H|T] = SpoutsConfigs, ResultList) when is_record(H, spoutConfig) ->
-	getSpoutsIdList(T, [{{H#spoutConfig.id}}|ResultList]).
+	getSpoutsIdList(T, [H#spoutConfig.id|ResultList]);
+getSpoutsIdList([],ResultList) ->
+	ResultList.
 
 getBoltsIdList(BoltsConfigs) ->
 	getBoltsIdList(BoltsConfigs,[]).
-getBoltsIdList([], ResultList) ->
-	ResultList;
 getBoltsIdList([H|T] = BoltsConfigs, ResultList) when is_record(H, boltConfig) ->
-	getBoltsIdList(T, [{H#boltConfig.id}|ResultList]).
+	getBoltsIdList(T, [H#boltConfig.id|ResultList]);
+getBoltsIdList([], ResultList) ->
+	ResultList.
 
 
 getConnPairsList(ConnsConfigs) ->
 	genConnPairsList(ConnsConfigs,[]).
-genConnPairsList([],ResultList)->
-	ResultList;
 genConnPairsList([H|T] = ConnsConfigs, ResultList) when is_record(H, connConfig)->
-	#connConfig{from = From, to = To} = H,
-	genConnPairsList(T, [{From, To}|ResultList]).
+	genConnPairsList(T, [{H#connConfig.from, H#connConfig.to}|ResultList]);
+genConnPairsList([],ResultList)->
+	ResultList.
 
 
