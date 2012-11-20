3b7da192a935333d080b5e8e1f345ab6c6d78061
Bowen Liang <simonlbw@gmail.com>
2012-11-21 01:58:47 +0800

    有关测试用例的小改。

--- src/ex.erl
+++ src/ex.erl
@@ -10,19 +10,13 @@
 %%
 %% Exported Functions
 %%
--export([start/0,ex/0]).
+-export([ex/0]).
 
 %%
 %% API Functions
 %%
-abc(Atom)->
-	Atom*2.
-ex()->
-	List1=[1,2],
-	Fun = fun (Atom)-> abc(Atom) end,
-	lists:foreach(Fun, List1).
 
-start()->
+ex()->
 	Topo = sardine:newTopo(),
 	Topo1 = sardine:setSpout(Topo, "a", "b"),
 	Topo2 = sardine:setSpout(Topo1, "c", "d",3),
@@ -31,7 +25,7 @@ start()->
 	Topo5 = sardine:shuffleGrouping(Topo4, "a", "e"),
 	Topo6 = sardine:setBolt(Topo5, "g", "h",4),
 	Topo7 = sardine:shuffleGrouping(Topo6, "e", "g"),
-	Cluster = sardine:cluster("127.0.0.1", 2181),
+	Cluster = sardine:cluster("192.168.204.128", 2181),
 	FeedBack = sardine:submitTopology(Cluster, Topo7),
 	file:write_file("TopoInfo.dat", term_to_binary(FeedBack)),
 	{ok,Data}=file:read_file("TopoInfo.dat"),
