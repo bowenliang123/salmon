6237f40c7bb32098bce6c0874205c6c2ade3f155
Bowen Liang <simonlbw@gmail.com>
2012-11-29 01:57:54 +0800

    增加BOLT_MSG_SUP, BOLT_MSG_HANDLER.

--- include/sm.hrl
+++ include/sm.hrl
@@ -3,7 +3,11 @@
 -define(BOLTS_SUP, sm_bolts_sup).
 -define(HOOK_SUP, sm_hook_sup).
 
+-define(BOLT_MSG_SUP, sm_boltmsg_sup).
+
 -define(SPOUT_ACTOR, sm_spout_actor).
 -define(BOLT_ACTOR, sm_bolt_actor).
+
+-define(BOLT_MSG_HANDLER, sm_boltmsg).
 
 -define(HOOK, sm_hook).
