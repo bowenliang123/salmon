17ad7cc18257c792be50565a49e29a1cfd8ffeb1
Bowen Liang <simonlbw@gmail.com>
2012-11-30 01:48:03 +0800

    调试之用。

--- src/ex.erl
+++ src/ex.erl
@@ -20,10 +20,10 @@
 
 ex()->
 	Topo = sm:newTopo("t1"),
-	Topo1 = sm:setSpout(Topo, "A", moduleA),
+	Topo1 = sm:setSpout(Topo, "A", ex_spout),
 	Topo2 = sm:setSpout(Topo1, "B", moduleB),
 	Topo3 = sm:setBolt(Topo2, "C", ex_bolt),
-	Topo4 = sm:setBolt(Topo3, "D", ex_bolt1),
+	Topo4 = sm:setBolt(Topo3, "D", ex_bolt1,2),
 	Topo5 = sm:shuffleGrouping(Topo4, "A", "C"),
 	Topo6 = sm:shuffleGrouping(Topo5, "B", "C"),
 	Topo7 = sm:shuffleGrouping(Topo6, "C", "D"),
@@ -38,7 +38,7 @@ xx()->
 	application:start(salmon).
 
 aa()->
-	ServerName=sm_utils:genServerName("t1", bolt, "C", 0),
+	ServerName=sm_utils:genServerName("t1", spout, "A", 0),
 	gen_server:cast(ServerName,{nextTuple,#tuple{content="lbw"},self()}).
 %%
 %% Local Functions
