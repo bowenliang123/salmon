2cab20e99ddfa24217ab9c2114f2cba4512e17e2
Bowen Liang <simonlbw@gmail.com>
2012-11-30 01:51:05 +0800

    一些有关msg的常量设置。

--- include/sm.hrl
+++ include/sm.hrl
@@ -3,11 +3,14 @@
 -define(BOLTS_SUP, sm_bolts_sup).
 -define(HOOK_SUP, sm_hook_sup).
 
--define(BOLT_MSG_SUP, sm_boltmsg_sup).
-
 -define(SPOUT_ACTOR, sm_spout_actor).
 -define(BOLT_ACTOR, sm_bolt_actor).
 
+
+-define(SPOUT_MSG_SUP, sm_spoutmsg_sup).
+-define(BOLT_MSG_SUP, sm_boltmsg_sup).
+
+-define(SPOUT_MSG_HANDLER, sm_spoutmsg).
 -define(BOLT_MSG_HANDLER, sm_boltmsg).
 
 -define(HOOK, sm_hook).
